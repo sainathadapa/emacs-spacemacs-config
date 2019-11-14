8884342b83457f3af43e55337f63949e318e8bb1
Sainath Adapa <adapasainath@gmail.com>
2019-11-14 17:23:57 +0100

    run grasp shell script

---
+++ run_grasp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/sainath/emacs-spacemacs-config/grasp/server/grasp_server.py --path /home/sainath/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]]\n** Selection\n%:selection\n** Comment\n%:comment\n'
+
