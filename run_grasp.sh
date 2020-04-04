831149eb1a63f49747d1f8b2637bcfa8d71387be
Sainath Adapa <sainathadapa@users.noreply.github.com>
2020-04-04 10:37:47 +0200

    Update the grasp template

--- run_grasp.sh
+++ run_grasp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-/home/sai/emacs-spacemacs-config/grasp/server/grasp_server.py --path /home/sai/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]]\n** Selection\n%:selection\n** Comment\n%:comment\n'
+# /Users/sainatha/emacs-spacemacs-config/grasp/server/grasp_server.py --path /Users/sainatha/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]]\n** Selection\n%:selection\n** Comment\n%:comment\n'
 
+# Default template
+# /Users/sainatha/emacs-spacemacs-config/grasp/server/grasp_server.py --path /Users/sainatha/Dropbox/org/grasp.org
+
+# /Users/sainatha/emacs-spacemacs-config/grasp/server/grasp_server.py --path /Users/sainatha/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]] %:tags \n** Selection\n%:selection\n** Comment\n%:comment\n'
+
+/Users/sainatha/emacs-spacemacs-config/grasp/server/grasp_server.py --path /Users/sainatha/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]] %:tags \n%:comment\n\n%:selection\n'
