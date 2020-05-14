6920ebdcd2d9d2ce8b89c1ca86b2a52844923082
Sainath Adapa <sainathadapa@users.noreply.github.com>
2020-05-14 17:48:31 +0200

    update grasp command

--- run_grasp.sh
+++ run_grasp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-/home/sai/emacs-spacemacs-config/grasp/server/grasp_server.py --path /home/sai/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]]\n** Selection\n%:selection\n** Comment\n%:comment\n'
-
+/home/sai/emacs-spacemacs-config/grasp/server/grasp_server.py --path /home/sai/Dropbox/org/grasp.org --template $'* TODO [#H] [[%:link][%:description]] %:tags \n%:comment\n\n%:selection\n'
